0730d3628155e748f5ce9e5e41d945f325aa5c5b
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-05-20 11:19:20 +0700

    feat: topic by slug

--- pages/[slug].vue
+++ pages/[slug].vue
@@ -1,8 +1,35 @@
+<script setup lang="ts">
+import { Database } from '~/types/database'
+
+const client = useSupabaseClient<Database>()
+const route = useRoute()
+// @ts-ignore
+const slug: string = route.params.slug
+
+const { data: topic } = await useAsyncData(slug, async () => {
+  const { data } = await client.from('topics').select('*').eq('slug', slug).single()
+
+  return data
+})
+
+useHead({
+  title: topic.value?.title,
+})
+</script>
+
 <template>
   <div class="container">
     <article class="article">
-      <h1 class="article-title">title</h1>
-      <p class="article-meta">Written by: anybody</p>
+      <h1 class="article-title">{{ topic?.title }}</h1>
+      <div class="article-meta">
+        <span> Issued by: anybody </span>
+        <time
+          :datetime="new Date(topic?.created_at).toISOString()"
+          :title="new Date(topic?.created_at).toLocaleString()"
+        >
+          {{ new Date(topic?.created_at).toLocaleDateString() }}
+        </time>
+      </div>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, placeat facere? Ad laudantium harum maiores?
         Nulla illum facere exercitationem debitis aut maiores. Pariatur voluptatibus harum eos doloremque magni error
@@ -11,3 +38,14 @@
     </article>
   </div>
 </template>
+
+<style scoped>
+.article-meta {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 1rem;
+  border-bottom: 1px solid #ccc;
+  padding-bottom: 1rem;
+}
+</style>
