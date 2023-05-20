3ac8be65fcf0d8ec8dd9a427aa39c73655d66205
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-05-20 09:01:38 +0700

    fix: only run middleware in server

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -1,11 +1,13 @@
-export default defineNuxtRouteMiddleware(async to => {
-  const user = useSupabaseUser()
+export default defineNuxtRouteMiddleware(to => {
+  if (process.server) {
+    const user = useSupabaseUser()
 
-  const authPages = ['/login', '/signup']
+    const authPages = ['/login', '/signup']
 
-  if (!user.value && !authPages.includes(to.path)) {
-    return '/login'
-  } else if (user.value && authPages.includes(to.path)) {
-    return '/'
+    if (!user.value && !authPages.includes(to.path)) {
+      return '/login'
+    } else if (user.value && authPages.includes(to.path)) {
+      return '/'
+    }
   }
 })
