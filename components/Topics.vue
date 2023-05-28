21459c243eb25c8ff2a646037aba7d5fd46f97cb
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-05-28 21:32:24 +0700

    feat: delete article

--- components/Topics.vue
+++ components/Topics.vue
@@ -21,12 +21,54 @@ const { data: topics } = await useAsyncData(props.userId ? `${props.userId}-topi
 
   return allTopics as Topic[]
 })
+
+async function deleteArticle(id: string) {
+  const isConfirm = confirm('Are you sure you want to delete this article?')
+  if (!isConfirm || !id) return
+
+  try {
+    await client.from('topics').delete().eq('id', id)
+  } catch (error) {
+    console.error(error)
+  } finally {
+    await refreshNuxtData(`${props.userId}-topics`)
+  }
+}
 </script>
 
 <template>
-  <ul>
-    <li class="margin-top-small" v-for="topic in topics" :key="topic.id">
-      <NuxtLink :to="'/topics/' + topic.slug">{{ topic.title }}</NuxtLink>
-    </li>
-  </ul>
+  <NuxtLink v-for="topic in topics" :key="topic.id" :to="'/topics/' + topic.slug">
+    <div class="card margin-top">
+      <div class="card-body">
+        <h4 class="card-title">
+          {{ topic.title }}
+        </h4>
+      </div>
+      <button @click.stop.prevent="deleteArticle(topic.id)" class="card-link btn-small btn-danger">🔥</button>
+    </div>
+  </NuxtLink>
 </template>
+
+<style scoped>
+.card {
+  position: relative;
+}
+
+a {
+  position: relative;
+  z-index: 1;
+}
+
+button {
+  position: absolute;
+  right: 0;
+  top: 0;
+  display: inline-flex;
+  align-items: center;
+  width: 30px;
+  height: 20px;
+  margin: 10px;
+  padding: 4px;
+  z-index: 10;
+}
+</style>
