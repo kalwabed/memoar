9061a613eb0f869021bdbbcd0a54556ade3f5b5c
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-07-27 21:22:29 +0700

    feat: initial new post page

--- pages/[username].vue
+++ pages/[username].vue
@@ -1,10 +1,6 @@
 <script setup lang="ts">
 import { Database } from '~/types/database'
 
-const editorValue = ref('')
-const title = ref('')
-const isAddPost = ref(false)
-
 const { user: userStore } = useAuthStore()
 const route = useRoute()
 const client = useSupabaseClient<Database>()
@@ -22,32 +18,6 @@ const isCurrentUser = computed(() => {
   return userStore?.value?.id === user?.value?.id
 })
 
-const onSubmit = async () => {
-  try {
-    const { data: post } = await client
-      .from(POSTS_TABLE)
-      .insert([
-        {
-          title: title.value,
-          user_id: userStore.value?.id,
-          content: editorValue.value,
-          slug: generateSlug(title.value),
-        },
-      ])
-      .select('id')
-      .single<{ id: string }>()
-    await client.from(POST_ANALYTICS_TABLE).insert({ post_id: post?.id })
-
-    editorValue.value = ''
-    title.value = ''
-    isAddPost.value = false
-  } catch (error) {
-    console.error(error)
-  } finally {
-    await refreshNuxtData(`${userStore.value?.id}-posts`)
-  }
-}
-
 useHead({
   title: username,
 })
@@ -57,23 +27,10 @@ useHead({
   <div class="container mt-8">
     <div v-if="user?.id">
       <ProfileMeta :user="user" />
-      <form @submit.prevent="onSubmit" v-if="isAddPost" class="mt-4 flex flex-col gap-4">
-        <div class="form-group">
-          <label for="title">Title</label>
-          <input type="text" id="title" class="input" v-model="title" />
-        </div>
-        <div class="form-group">
-          <label for="content">Content</label>
-          <LazyEditor contentType="html" id="content" v-model:content="editorValue" />
-        </div>
-        <div class="mt-4 inline-flex gap-3">
-          <button class="btn-gray py-1 text-sm" @click.stop.prevent="isAddPost = false">Cancel</button>
-          <button type="submit" class="btn-teal py-1 text-sm">Send</button>
-        </div>
-      </form>
+
       <div class="flex items-center justify-between">
         <h4 v-if="isCurrentUser" class="my-4 text-xl font-bold leading-relaxed">Your Posts</h4>
-        <button v-if="isCurrentUser" v-show="!isAddPost" @click="isAddPost = true" class="btn-blue">Add Post</button>
+        <NuxtLink v-if="isCurrentUser" to="/new" class="btn-blue btn-sm">Write New Post</NuxtLink>
       </div>
       <Suspense>
         <Posts :enable-delete="isCurrentUser" :user-id="user?.id" />
