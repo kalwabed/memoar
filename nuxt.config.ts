18c310c89f4806a6753bc5bfd936aab00ddfc8be
kalwabed <49640654+kalwabed@users.noreply.github.com>
2023-07-30 19:58:32 +0700

    chore: formatting

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,45 +2,35 @@
 export default defineNuxtConfig({
   css: ['@unocss/reset/tailwind-compat.css', 'primevue/resources/themes/lara-light-teal/theme.css'],
   modules: ['@vueuse/nuxt', '@nuxtjs/supabase', '@unocss/nuxt'],
-
   build: {
     transpile: ['primevue'],
   },
-
   experimental: {
     typedPages: true,
     viewTransition: true,
     typescriptBundlerResolution: true,
   },
-
   imports: {
     dirs: ['./store'],
   },
-
   nitro: {
     prerender: {
       crawlLinks: true,
     },
   },
-
   devtools: {
     enabled: true,
   },
-
   runtimeConfig: {
     public: {
       supabaseKey: process.env.NUXT_PUBLIC_SUPABASE_KEY,
     },
   },
-
   vue: {
     propsDestructure: true,
     defineModel: true,
   },
-
   typescript: {
     strict: false,
   },
-
-  plugins: ['~/plugins/primevue.ts'],
 })
